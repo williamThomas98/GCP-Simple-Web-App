494c97d851201dc00106070d46c850286941efb0
william <william_thomas@bca.co.id>
2022-05-17 07:52:36 +0000

    update source code

--- app.js
+++ app.js
@@ -22,11 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/crash', crashRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
